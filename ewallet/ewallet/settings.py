6771a0cbe5739e28e7801edf9c00989902acd424
Pavel Loginov <pavel.loginov.dev@gmail.com>
2021-04-01 20:18:05 +0300

    Disable debug.

--- ewallet/ewallet/settings.py
+++ ewallet/ewallet/settings.py
@@ -13,7 +13,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'127.0.0.1',
+	'localhost',
+]
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
